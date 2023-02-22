4d1dc70e2c5c099d46cb65864655c8d093d3be2b
kos <ustyugovki@bk.ru>
2023-02-22 17:31:31 +0300

    Разметка блока Promo для Mobile

--- source/js/script.js
+++ source/js/script.js
@@ -14,3 +14,29 @@ toggle.addEventListener('click', function () {
     menu.classList.remove('menu--opened');
   }
 });
+
+
+/* Swiper */
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
